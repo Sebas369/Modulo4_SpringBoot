4a2dcf70f4559c9b3f098cd4538d65911a3d2d92
Sebastian Rosales <sebar126@gmail.com>
2020-01-17 01:25:00 -0300

    Se agregó el CRUD completo. Se agregó la clase service

--- src/main/java/com/springboot/mod4/app/dao/ClienteDaoImpl.java
+++ src/main/java/com/springboot/mod4/app/dao/ClienteDaoImpl.java
@@ -7,7 +7,6 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.springboot.mod4.app.models.entities.Cliente;
 
@@ -19,17 +18,31 @@ public class ClienteDaoImpl implements IClienteDao {
 	EntityManager em;
 
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
 	@Override
 	public List<Cliente> findAll() {
 		
 		return em.createQuery("from Cliente").getResultList();
 	}
 
-	@Transactional
+
 	@Override
 	public void save(Cliente cliente) {
-		em.persist(cliente);
+		
+		if(cliente.getId() != null && cliente.getId() > 0) {
+			em.merge(cliente);
+		}else {
+			em.persist(cliente);
+		}
+	}
+
+	@Override
+	public Cliente findOne(Long id) {
+		return em.find(Cliente.class, id);
+	}
+
+	@Override
+	public void deleteOne(Long id) {
+		em.remove(findOne(id));
 		
 	}
 
